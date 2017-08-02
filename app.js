ecabe4ca03e8f60595cc3f57f2dec4f6fb73cca5
rabbit159t <b00902045@ntu.edu.tw>
2017-08-03 03:12:14 +0800

    bibile

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 
 var index = require('./routes/index');
+var bible = require('./routes/bible');
 var users = require('./routes/users');
 
 var app = express();
@@ -25,24 +26,25 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/bible', bible);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
---
+++ routes/bible.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+var express = require('express');
+var router = express.Router();
+
+var oldTestament = [], newTestament = [];
+content = require(path.join(__dirname, 'content.json'));
+for (var i = 0, flag = false; i < content["en"].length; i++) {
+    if (flag || content["en"][i] == "Matthew") {
+        flag = true;
+        newTestament.push(content["en"][i]);
+    }
+    else{
+        oldTestament.push(content["en"][i]);
+    }
+}
+console.log(oldTestament);
+console.log(newTestament);
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('bible', {
+        title: 'Yee',
+        old: oldTestament,
+        new: newTestament
+    });
+});
+
+module.exports = router;
