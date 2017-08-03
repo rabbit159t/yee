fbb27b6be1b1b04a9d7f30fa175b655ce29224cc
rabbit159t <b00902045@ntu.edu.tw>
2017-08-03 22:13:09 +0800

    text

--- routes/bible.js
+++ routes/bible.js
@@ -5,12 +5,14 @@ var router = express.Router();
 
 var oldContent = require(path.join(__dirname, 'oldContent.json'));
 var newContent = require(path.join(__dirname, 'newContent.json'));
+var bible = require(path.join(__dirname, 'bible.json'));
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('bible', {
         title: 'Yee',
         old: oldContent,
-        new: newContent
+        new: newContent,
+        bible: JSON.stringify(bible)
     });
 });
 
