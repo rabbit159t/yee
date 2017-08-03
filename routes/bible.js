ee20f0ddec157eeb530a509b3140f2cfe47b780c
rabbit159t <b00902045@ntu.edu.tw>
2017-08-03 17:51:57 +0800

    content

--- routes/bible.js
+++ routes/bible.js
@@ -3,23 +3,14 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-var oldTestament = [], newTestament = [];
-content = require(path.join(__dirname, 'content.json'));
-for (var i = 0, flag = false; i < content["en"].length; i++) {
-    if (flag || content["en"][i] == "Matthew") {
-        flag = true;
-        newTestament.push(content["en"][i]);
-    }
-    else{
-        oldTestament.push(content["en"][i]);
-    }
-}
+var oldContent = require(path.join(__dirname, 'oldContent.json'));
+var newContent = require(path.join(__dirname, 'newContent.json'));
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('bible', {
         title: 'Yee',
-        old: oldTestament,
-        new: newTestament
+        old: oldContent,
+        new: newContent
     });
 });
 
